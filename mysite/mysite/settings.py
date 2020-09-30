d9d97a7673f15d9f40dd0ad51ba1df9b6f0abc7d
Douglas Morais <douglas.morais@grupogreencard.com.br>
2020-09-30 15:04:58 -0300

    Update staticfiles

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import django-heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
