069bcff68818e8cddcc69030593d91615fe8ade6
Douglas Morais <douglas.morais@grupogreencard.com.br>
2020-09-30 16:50:15 -0300

    Adjust Db config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,5 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')}
