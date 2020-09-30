45a99785b9a753273bcb8afbdc1dbb9c1c8ed762
Douglas Morais <douglas.morais@grupogreencard.com.br>
2020-09-30 16:36:18 -0300

    Adjust Db config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
